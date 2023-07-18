21140567a38cb47ad4c2eeebb6f68f98be2f8be4
Malavath Shekar <89739461+malavathshekar123@users.noreply.github.com>
2023-07-18 15:22:51 +0530

    Add files via upload

---
+++ php/footer.php
@@ -0,0 +1,46 @@
+<footer id="footer" class="footer">
+
+
+
+    <div class="footer-legal text-center">
+        <div class="container d-flex flex-column flex-lg-row justify-content-center justify-content-lg-between align-items-center">
+
+            <div class="d-flex flex-column align-items-center align-items-lg-start">
+                <div class="copyright">
+                    <strong> Group Members:</strong> Amartya Raj, Ajinkya Kale, Kushalava, Malavath Shekar and Mohit Kumar.
+                </div>
+                <div class="credits">
+                    <h5>
+
+                    <a>DBMS Lab Project</a>
+</h5>
+                </div>
+            </div>
+
+            <div class="social-links order-first order-lg-last mb-3 mb-lg-0">
+                <a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwj2jLOL7qH-AhWpcGwGHa8VBDgQFnoECA0QAQ&url=https%3A%2F%2Fwww.nitdgp.ac.in%2F&usg=AOvVaw1Dvno5ngV-1J0FoZKE8pBF" class="twitter"><i class="bi bi-twitter"></i></a>
+                <a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwj2jLOL7qH-AhWpcGwGHa8VBDgQFnoECA0QAQ&url=https%3A%2F%2Fwww.nitdgp.ac.in%2F&usg=AOvVaw1Dvno5ngV-1J0FoZKE8pBF" class="facebook"><i class="bi bi-facebook"></i></a>
+                <a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwj2jLOL7qH-AhWpcGwGHa8VBDgQFnoECA0QAQ&url=https%3A%2F%2Fwww.nitdgp.ac.in%2F&usg=AOvVaw1Dvno5ngV-1J0FoZKE8pBF" class="instagram"><i class="bi bi-instagram"></i></a>
+                <a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwj2jLOL7qH-AhWpcGwGHa8VBDgQFnoECA0QAQ&url=https%3A%2F%2Fwww.nitdgp.ac.in%2F&usg=AOvVaw1Dvno5ngV-1J0FoZKE8pBF" class="google-plus"><i class="bi bi-skype"></i></a>
+                <a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwj2jLOL7qH-AhWpcGwGHa8VBDgQFnoECA0QAQ&url=https%3A%2F%2Fwww.nitdgp.ac.in%2F&usg=AOvVaw1Dvno5ngV-1J0FoZKE8pBF" class="linkedin"><i class="bi bi-linkedin"></i></a>
+            </div>
+
+        </div>
+    </div>
+
+</footer>
+
+<a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<div id="preloader"></div>
+
+<!-- Vendor JS Files -->
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendor/aos/aos.js"></script>
+<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="assets/vendor/php-email-form/validate.js"></script>
+
+<!-- Template Main JS File -->
+<script src="assets/js/main.js"></script>
